049b8c3b85553bb2d7dc30d242b7e20526428a80
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-10 21:58:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,19 +7,19 @@ class School
     @roster = {}
   end 
   
-  def add_student(student, grades)
+  def add_student(student, grades) #adding students to roster hash
     roster[grades] ||= []
     roster[grades] << student
   end 
   
-  def grade(grades)
+  def grade(grades) #returning information in the roster hash
     roster.detect do |x, y| 
       if x == grades
         return y 
       end 
     end 
   end 
-  def sort 
+  def sort #sorting information in the roster hash
     hash = {}
     roster.each do |x, y| 
       hash[x] = y.sort 
