e7994462499955b9b0d7490b96dce19db0940cef
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-10 21:44:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,24 @@
 # code here!
 class School
+  attr_accessor :name, :roster
   
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student, level)
+    # this sort of means #=> a || a = b ... or #=> x || x = y
+    # it's freak'n weird
+    roster[level] ||= []
+    roster[level] << student
+  end 
+  
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
+  end 
 end
